f779e50ff16fa7f1c8b702c078abc625f4f7c157
drake may <drake@may.com>
2026-08-26 20:39:16 +0000

    Initialized backend by using Spring Boot Initializer to create Java backend.

---
+++ src/test/java/com/ceo/trading_platform_backend/TradingPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceo.trading_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
